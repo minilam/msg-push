95e6a90767bb8c31d4e592dceb7abb1012c24db2
hongliang <liuforworking@163.com>
2019-06-21 19:00:29 +0800

    feat: merge

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+'use strict'
+module.exports = {
+  url: "http://xiaoc.easy.echosite.cn/", // 调用的api服务器域名
+  redis:{
+    url: 'localhost',
+    port: '63790',
+    password:'',
+    select: 3
+}
+}
